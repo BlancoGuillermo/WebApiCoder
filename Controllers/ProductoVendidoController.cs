16c59845650738102d122efd27dd4a32e532b5ab
BlancoGuillermo <105031998+BlancoGuillermo@users.noreply.github.com>
2023-02-14 23:00:04 -0300

    Primera version

---
+++ Controllers/ProductoVendidoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCoder.Models;
+using WebApiCoder.Repository;
+
+namespace MiPrimeraAppV1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoVendidoController : ControllerBase
+    {
+       
+    }
+
+}
+
